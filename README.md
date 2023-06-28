e578fb323dbf939593b4535fbb445af6023230e9
Luis Noguera <84802276+silimotion@users.noreply.github.com>
2023-06-28 17:47:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,89 @@
+# brotsycl
+
+brotsycl is a CLI utility that allows to create .ppm images of the Mandelbrot set, for scientists and artists. Leveraging the SYCL language, it can be run
+on CPUs and GPUs, allowing for very fast computation.
+
+## Usage
+
+The executable is to be run from the command line. You'll need to provide the coordinates of the bottom-left pixel, the size in the vertical axis, the size of the generated image in pixels and the max number of iterations. There are also some options, for example to force cpu usage.
+
+This is an example command:
+```bash
+./brotsycl -2 -2 4 1024 1024 1000
+```
+It produces a square 1024x1024 image with (-2, -2) bottom-left, and (2, 2) upper-right pixels.
+
+After doing the heavy calculations, a buffer file is created. It can be fed to the program with -i to skip the computation phase.
+
+There will be an all-architectures executable in releases Soon™. View [#Limitations](#limitations) for more info
+
+## Building
+
+You first need a SYCL compiler, such as [OpenSYCL](https://github.com/OpenSYCL/OpenSYCL) or DPC++. If you wish to have gpu acceleration, 
+you'll need the appropiate drivers installed, and compile with a capable compiler.
+
+Once you have a functional installation you can compile the project. If using the OpenSYCL compiler, you may compile using the following command:
+```bash
+syclcc --opensycl-targets="omp;generic" -Ofast -flto -o brotsycl main.cpp 
+```
+## Examples and Gallery
+
+```bash
+./brotsycl -0.71580045904 0.284632300657 0.000000000143 2048 2048 300000 --exp 0.7
+```
+![logo](brot1.png)
+
+Example usage of -i and --buf options:
+
+```bash
+#First create an image, saving it using --buf:
+./brotsycl -0.652 0.479 0.001 4096 4096 10000 --buf buffer.buf
+```
+![logo](example3.png)
+
+```bash
+#Now we use the previous buffer to create a new image without needing to calculate again:
+./brotsycl -i buffer.buf --exp 0.7 --bg 200 200 200 -o end_result.ppm
+```
+![logo](end_result.png)
+
+Another example of different results using --exp:
+
+```bash
+./brotsycl -0.71580045904 0.284632300943 0.000000000143 2048 2048 300000
+```
+![logo](example1.png)
+
+```bash
+./brotsycl -0.71580045904 0.284632300943 0.000000000143 2048 2048 300000 --exp 0.7
+```
+![logo](example2.png)
+
+## Benchmarks
+
+These benchmarks were measured using the 'time' utility ("user" tab), and compiling with the OpenSYCL compiler
+
+| Device                 | Image 1      | Image 2      |
+|------------------------|--------------|--------------|
+|  AMD 7600 CPU (OpenMP) | 14m 16.409s  |     57.380s  |
+|  AMD 6600 GPU          |      9.906s  |      1.269s  |
+|  AMD 7600 iGPU         |  2m 34.451s  |      9.509s  |
+
+## Limitations
+
+Due to the very early stage of SYCL compilers, it is hard to get a generic SYCL executable. Work is being done by the OpenSYCL team to 
+develop the SSCP generic compilation. Until everything is polished enough, it will be difficult to produce a tiny and performant downloadable executable runnable on all architectures.
+
+Using the GPU that runs the displays for the calculations may cause freezing. I am investigating this to clarify whether it is a bug in SYCL, the compiler, the program, or intended behaviour.
+
+When running on OpenMP some cores may not be used. I am investigating this also.
+
+The precision of the floats is double, as 128 bit floats are not supported in many GPUs, and will run **very** slowly in 64-bit CPUs. This shouldn't be a limitation in all but the most extreme cases. An example of this can be seen with the image obteined by running the following command: 
+```bash
+./brotsycl -0.71580045904 -0.2846323008 0.0000000000000143 2048 2048 300000 --gpu --exp 0.7
+```
+(Guess we'll have to wait till the 128 bit era)
+
+## License
+
+This repository is licensed under the [MIT license](LICENSE.md)
---
+++ main.cpp
@@ -0,0 +1,318 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <CL/sycl.hpp>
+#include <iostream>
+#include <optional>
+#include <fstream>
+
+using namespace cl::sycl;
+
+typedef struct Pixel
+{
+    char r;
+    char g;
+    char b;
+} Pixel;
+
+void invalid_input_print();
+
+int main(int argc, char *argv[])
+{
+    double x_size, y_size;            // Size of each pixel
+    int canvas_x_size, canvas_y_size; // Size of the canvas
+    double x_offset, y_offset;        // Left-down corner of the image
+    int p;                            // precision (max number of cycles)
+    double power = 1;                 // exponent
+    bool force_gpu = false;
+    bool force_cpu = false;
+    char *filename = NULL;
+    char *filename_buffer = NULL;
+    char *filename_input = NULL;
+    Pixel bg = {0, 0, 0};
+    cl::sycl::queue q;
+    std::optional<buffer<int, 2>> D;
+
+    if (platform::get_platforms().empty() == true)
+    {
+        std::cout << "No platforms found - Install appropiate drivers";
+        exit(-1);
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "--help") == 0)
+        {
+            invalid_input_print();
+            exit(0);
+        }
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "--gpu") == 0)
+        {
+            force_gpu = true;
+        }
+        else if (strcmp(argv[i], "--cpu") == 0)
+        {
+            force_cpu = true;
+        }
+        else if (strcmp(argv[i], "--exp") == 0)
+        {
+            if (argc < (i + 2))
+            {
+                invalid_input_print();
+                exit(-1);
+            }
+            power = atof(argv[i + 1]);
+        }
+        else if (strcmp(argv[i], "-o") == 0)
+        {
+            if (argc < (i + 2))
+            {
+                invalid_input_print();
+                exit(-1);
+            }
+            filename = argv[i + 1];
+        }
+        else if (strcmp(argv[i], "-i") == 0)
+        {
+            if (argc < (i + 2))
+            {
+                invalid_input_print();
+                exit(-1);
+            }
+            filename_input = argv[i + 1];
+        }
+        else if (strcmp(argv[i], "--buf") == 0)
+        {
+            if (argc < (i + 2))
+            {
+                invalid_input_print();
+                exit(-1);
+            }
+            filename_buffer = argv[i + 1];
+        }
+        else if (strcmp(argv[i], "--bg") == 0)
+        {
+            if (argc < (i + 4))
+            {
+                invalid_input_print();
+                exit(-1);
+            }
+
+            bg.r = (atoi(argv[i + 1]) % 256);
+            bg.g = (atoi(argv[i + 2]) % 256);
+            bg.b = (atoi(argv[i + 3]) % 256);
+        }
+        else if (strcmp(argv[i], "-l") == 0)
+        {
+            for (auto const &this_platform : platform::get_platforms())
+            {
+                std::cout << "Found platform: " << this_platform.get_info<info::platform::name>() << std::endl;
+                // Loop through available devices in this platform
+                for (auto const &this_device : this_platform.get_devices())
+                {
+                    std::cout << " Device: " << this_device.get_info<info::device::name>() << std::endl;
+                }
+                std::cout << "\n";
+            }
+            exit(0);
+        }
+    }
+
+    if (force_gpu == true)
+    {
+        q = queue{gpu_selector()};
+    }
+    else if (force_cpu == true)
+    {
+        q = queue{cpu_selector()};
+    }
+    else
+    {
+        q = queue{default_selector()};
+    }
+    std::cout << "Running on device: " << q.get_device().get_info<info::device::name>().c_str() << std::endl;
+
+    if (filename == NULL)
+    {
+        filename = (char *)"brot1.ppm";
+    }
+    std::ofstream f(filename);
+
+    if (filename_input == NULL)
+    {
+
+        if (argc < 7)
+        {
+            invalid_input_print();
+            exit(-1);
+        }
+
+        x_offset = atof(argv[1]);
+        y_offset = atof(argv[2]);
+        canvas_x_size = atof(argv[5]);
+        canvas_y_size = atof(argv[4]);
+        x_size = atof(argv[3]) / (double)canvas_x_size;
+        y_size = x_size;
+        p = atoi(argv[6]);
+
+        if (x_size <= 0 || p <= 0 || canvas_x_size <= 0 || canvas_y_size <= 0 || power <= 0 || bg.r < 0 || bg.g < 0 || bg.b < 0)
+        {
+            invalid_input_print();
+            exit(-1);
+        }
+        D.emplace(range(canvas_x_size, canvas_y_size));
+
+        q.submit([&](handler &h)
+                 {
+        accessor acc{D.value(), h, write_only, no_init};
+        h.parallel_for(range(canvas_x_size, canvas_y_size),[=] (id<2> idx)
+        {
+            // Mandelbrot series uses z_1 = z_0^2 + c
+            double c_real, c_imaginary; //real and imaginary parts of the point we are calculating
+            double z_real, z_imaginary; //real and imaginary of the result
+            int flag;
+
+            acc[idx] = -1;
+            z_real =z_imaginary = flag = 0;
+
+            c_real =        x_offset+x_size*idx[1];
+            c_imaginary =   y_offset+y_size*canvas_x_size-y_size*idx[0];
+
+            for (int i = 0; i < p && flag == 0; i++)
+            {
+            //We need to save the z_real value for computing z_imaginary
+                double z_real_buf;
+                z_real_buf = z_real;
+                z_real = z_real*z_real - z_imaginary*z_imaginary +c_real;
+                z_imaginary =  2*z_real_buf*z_imaginary+c_imaginary;
+                if ((z_real > 2) || (z_imaginary > 2))
+                {
+                    acc[idx] = i;
+                    flag = 1;
+                }
+            }
+        }); });
+
+        q.wait();
+        if (filename_buffer == NULL)
+        {
+            filename_buffer = (char *)"buffer.buf";
+        }
+        std::ofstream g(filename_buffer);
+        if (!g)
+        {
+            std::cout << "Error opening file: " << filename_buffer << std::endl;
+            exit(-1);
+        }
+
+        g.write((char *)&canvas_x_size, sizeof(int));
+        g.write((char *)&canvas_y_size, sizeof(int));
+        auto buffer = D.value().get_host_access(read_only);
+        for (int i = 0; i < canvas_x_size; i++)
+        {
+            for (int j = 0; j < canvas_y_size; j++)
+            {
+                g.write((char *)&buffer[i][j], sizeof(int));
+            }
+        }
+        g.close();
+    }
+    else
+    {
+        std::ifstream input_file(filename_input, std::ios::binary);
+        if (!input_file)
+        {
+            std::cout << "Error opening file: " << filename_input << std::endl;
+            exit(-1);
+        }
+
+        input_file.read((char *)&canvas_x_size, sizeof(canvas_x_size));
+        input_file.read((char *)&canvas_y_size, sizeof(canvas_y_size));
+
+        if (canvas_x_size <= 0 || canvas_y_size <= 0)
+        {
+            std::cout << "Invalid file" << std::endl;
+            input_file.close();
+            exit(-1);
+        }
+        try
+        {
+            D.emplace(range(canvas_x_size, canvas_y_size));
+        }
+        catch(const hipsycl::sycl::runtime_error& e)
+        {
+            std::cout << "Error processing file" << std::endl;
+            exit(-1);
+        }
+
+        auto input = D.value().get_host_access(write_only, no_init);
+        for (int i = 0; i < canvas_x_size; i++)
+        {
+            for (int j = 0; j < canvas_y_size; j++)
+            {
+                try
+                {
+                    input_file.read((char *)&input[i][j], sizeof(int));
+                }
+                catch (std::ifstream::failure &e)
+                {
+                    std::cout << "Invalid file" << std::endl;
+                    exit(-1);
+                }
+            }
+        }
+        input_file.close();
+    }
+    buffer<Pixel, 2> C(range(canvas_x_size, canvas_y_size));
+    q.submit([&](handler &h)
+             {
+        accessor acc{C, h, write_only, no_init};
+        accessor acc2{D.value(), h, read_only};
+        h.parallel_for(range(canvas_x_size, canvas_y_size), [=] (id<2> idx) {
+            if (acc2[idx]==-1)
+            {
+                acc[idx].r = bg.r; //Set the initial value for the points, 0 means it has not diverged
+                acc[idx].g = bg.g;
+                acc[idx].b = bg.b;
+            }
+            else
+            {
+                acc[idx].r = (long)hipsycl::sycl::powr((double)acc2[idx], power)%256;
+                acc[idx].g = 0;
+                acc[idx].b = 0;
+            }
+            
+        }); });
+    q.wait();
+    f << "P6 " << canvas_y_size << " " << canvas_x_size << " 255" << std::endl;
+    auto result = C.get_host_access(read_only);
+    for (int i = 0; i < canvas_x_size; i++)
+    {
+        for (int j = 0; j < canvas_y_size; j++)
+        {
+            f.write((char *)&result[i][j], sizeof(Pixel));
+        }
+    }
+    f.close();
+}
+
+void invalid_input_print()
+{
+    std::cout << "Usage: brotsycl x_offset y_offset canvas_h_size canvas_v_size cycles [option]..." << std::endl;
+    std::cout << "Create a .ppm image of the mandelbrot set at the given coordinates" << std::endl
+              << std::endl;
+    std::cout << "Options:" << std::endl;
+    std::cout << "  --cpu               Force the use of cpu instead of other accelerators" << std::endl;
+    std::cout << "  --gpu               Force the use of gpu instead of other accelerators" << std::endl;
+    std::cout << "  -l                  List all platforms and devices available" << std::endl;
+    std::cout << "  -o <filename>       Save the produced image to the indicated filename (by default brot1.ppm)" << std::endl;
+    std::cout << "  --exp <exponent>    The exponent used in the powr function to calculate the color for each pixel," << std::endl
+              << "                      use values lower than 1 to make slopes in color slower and avoid \"soup\" images." << std::endl;
+    std::cout << "  --bg <r> <g> <b>    Sets the color of the pixels that belong to the set (by default pitch black)" << std::endl;
+    std::cout << "  --buf <filename>    Sets the name of the file the buffer will be saved in" << std::endl;
+    std::cout << "  -i <filename>       Converts the given buffer file to a .ppm image, ignoring image inputs" << std::endl;
+    std::cout << "  --help              Show this help text" << std::endl
+              << std::endl;
+    return;
+}
